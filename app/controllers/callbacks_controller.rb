b331057fdf223f3aa0d7ad6b46594d218a0748d1
Vyacheslav <v.nichkov@hotmail.com>
2021-04-14 18:45:25 +0600

    fix bag with provider

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -2,7 +2,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
   def telegram
     req = request.env["omniauth.auth"]
     req[:provider] = :telegram
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(req)
     sign_in_and_redirect @user
   end
 end
