e9964d101350a7a2d124c042e01646b8fe02253a
Vyacheslav <v.nichkov@hotmail.com>
2021-04-14 17:08:01 +0600

    add   def self.(auth)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:telegram]
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+  def self.(auth)
+    where(provider: auth.provider, uid: auth.id).first_or_create do |user|
+      user.provider = auth.provider || :telegram
+      user.uid = auth.id
+      #user.email = auth.info.email
+      user.password = auth.hash
     end
   end
 
