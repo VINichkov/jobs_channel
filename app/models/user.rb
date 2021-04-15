8f2b1547ab2f33216ba9b0b957db858ae55506cd
Vyacheslav <v.nichkov@hotmail.com>
2021-04-15 10:38:26 +0600

    fix bag with provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
     where(provider: :telegram, uid: auth.id).first_or_create do |user|
       user.provider = :telegram
       user.uid = auth.id
+      user.first_name auth.first_name
+      user.last_name auth.last_name
       #user.email = auth.info.email
       user.password = auth.hash
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_085407) do
+ActiveRecord::Schema.define(version: 2021_04_14_154956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 2021_04_14_085407) do
     t.integer "salary_max"
     t.integer "job_type", default: 0, null: false
     t.string "contact"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["fts"], name: "index_jobs_on_fts", using: :gin
   end
 
