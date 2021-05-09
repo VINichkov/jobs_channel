9316ce243f47b002bf8aedc040ed078ec339d2e0
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2021-05-08 23:57:42 -0700

    fix bag callback class

--- app/keyboard/callbacks.rb
+++ app/keyboard/callbacks.rb
@@ -2,18 +2,19 @@ module Callbacks
   class Like
     LIKE = 'like'
     attr_reader :text, :id
-    def initialize(text = nil)
+    def initialize(text = nil, id: nil)
       if text.present?
         begin
           params = JSON.parse(text, opts={symbolize_names:true})
           @action = params[:action]
           @count = params[:count]
           @text = params[:text]
+          @id = params[:id]
         rescue
           Rails.logger.error("Like::Error: initialize text = #{text} : text error #{$!} ")
         end
       else
-        @count, @action, @text  = 0, LIKE, Emoji::LIKE
+        @count, @action, @text, @id  = 0, LIKE, Emoji::LIKE, id
       end
     end
 
@@ -28,7 +29,8 @@ module Callbacks
     def to_callback
       {action: @action,
        count: @count,
-       text: @count > 0 ? Emoji::LIKE + " #{@count}" : Emoji::LIKE
+       text: @count > 0 ? Emoji::LIKE + " #{@count}" : Emoji::LIKE,
+       id: @id
       }.to_json
     end
 
