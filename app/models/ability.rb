7ffc5c09664b39e87253d033a1258adcba75e4d5
Vyacheslav Nichkov <v.nichkov@hotmail.com>
2021-05-26 21:49:28 -0700

    fix bag approve

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,6 +7,8 @@ class Ability
     if user&.superadmin?
       can :manage, :all
     end
+
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
